55d9fcb453f3fd9eaa75d750f37d9f3bb286b0f5
紫风铃 <@WIN>
2018-03-11 16:56:22 +0800

    fix- 权限管理服务
    Change-Id: Ia27a536fdeb21033e9bede6a6180464b2fb168c2
    Signed-off-by: 紫风铃 <紫风铃@WIN>

--- src/cn/ys/dao/PrivilegeDao.java
+++ src/cn/ys/dao/PrivilegeDao.java
@@ -1,53 +1,13 @@
 package cn.ys.dao;
 
-import cn.ys.vo.Business;
-
 /**
- * 数据库-商家
+ * 数据库-用户权限设置
  * 
  * @author Administrator
  *
  */
 public interface PrivilegeDao {
 
-	/**
-	 * 添加商家
-	 * 
-	 * @param business
-	 *            商家对象
-	 * @return true-添加成功；false-添加失败
-	 */
-	boolean save(Business business);
 
-	/**
-	 * 删除商家表对应信息
-	 * 
-	 * @param username
-	 *            用户名
-	 * @return true-删除成功；false-删除失败
-	 */
-	boolean delUser(String username, String password);
-
-	/**
-	 * 通过用户名，旧密码修改为新密码
-	 * 
-	 * @param username
-	 *            用户名
-	 * @param oldPassword
-	 *            旧密码，用于内部校验
-	 * @param newPassword
-	 *            新密码
-	 * @return true-修改成功；false-修改成功
-	 */
-	boolean updatePassword(String username, String oldPassword, String newPassword);
-
-	/**
-	 * 通过用户名获取用户信息，包括密码
-	 * 
-	 * @param username
-	 *            用户名
-	 * @return user对象
-	 */
-	Business getBusinessByID(String username);
 
 }
